dd75fbf0f784494b060bdde841ff08b64d8a3ffd
Yousuf Khan <24100286@lums.edu.pk>
2024-06-23 20:54:13 +0500

    fix(database) - schema name made constant public

--- src/database/config.ts
+++ src/database/config.ts
@@ -2,24 +2,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const dbConfig = (() => {
-  const env = process.env.NODE_ENV ? process.env.NODE_ENV : (() => { throw new Error('NODE_ENV environment variable not set!') })()
   const connectionString = process.env.DATABASE_URL ? process.env.DATABASE_URL : (() => { throw new Error('DATABASE_URL environment variable not set!') })()
-  const schemaName = (() => {
-    switch (env) {
-      case 'development':
-        return 'awes-dev';
-        break;
-      case 'test':
-        return 'awes-test';
-        break;
-      case 'production':
-        return 'awes';
-        break;
-      default:
-        throw new Error('Invalid NODE_ENV! Please set the NODE_ENV environment variable to either "development", "test", or "production"!');
-    }
-  })()
-
+  const schemaName = 'public';
   return {
     connectionString,
     schemaName
