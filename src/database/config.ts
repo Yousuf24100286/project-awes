2bbd285771db5f27f3d1102424bfdf7de8b37340
Yousuf Khan <24100286@lums.edu.pk>
2024-06-23 21:03:14 +0500

    fix(database) - change schema to "awes_schema"
    - change schema to "awes_schema" in database/config.ts
    due to postgres constraints

--- src/database/config.ts
+++ src/database/config.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 const dbConfig = (() => {
   const connectionString = process.env.DATABASE_URL ? process.env.DATABASE_URL : (() => { throw new Error('DATABASE_URL environment variable not set!') })()
-  const schemaName = 'public';
+  const schemaName = 'awes_schema';
   return {
     connectionString,
     schemaName
