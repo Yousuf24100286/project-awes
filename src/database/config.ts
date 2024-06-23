be1346bce42bfcc95861e9d0735853159956cbb9
Yousuf Khan <24100286@lums.edu.pk>
2024-06-23 16:06:43 +0500

    setup(database) - add database/config.ts
    - add dynamic database configuration
    - add environment based schema selection (development, test, production)
    - ensure necessary environment variables are set (DATABASE_URL, NODE_ENV)

---
+++ src/database/config.ts
@@ -0,0 +1,26 @@
+const dbConfig = (() => {
+  const env = process.env.NODE_ENV ? process.env.NODE_ENV : (() => { throw new Error('NODE_ENV environment variable not set!') })()
+  const connectionString = process.env.DATABASE_URL ? process.env.DATABASE_URL : (() => { throw new Error('DATABASE_URL environment variable not set!') })()
+  const schemaName = (() => {
+    switch (env) {
+      case 'development':
+        return 'awes-dev';
+        break;
+      case 'test':
+        return 'awes-test';
+        break;
+      case 'production':
+        return 'awes';
+        break;
+      default:
+        throw new Error('Invalid NODE_ENV! Please set the NODE_ENV environment variable to either "development", "test", or "production"!');
+    }
+  })()
+
+  return {
+    connectionString,
+    schemaName
+  }
+})()
+
+export default dbConfig;
